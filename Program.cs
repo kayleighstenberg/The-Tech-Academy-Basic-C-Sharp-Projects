bf4dd97ba49660dded427b930a651cf404d1dcd4
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-10-22 15:39:39 -0700

    The Tech Academy Daily Report Drill

--- Program.cs
+++ Program.cs
@@ -1,13 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
+namespace The_Tech_Academy_Daily_Report
+{
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we're 'accounting' on you!");
-        Console.ReadLine();
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int intPageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'.");
+            string needsHelp = Console.ReadLine();
+            Console.WriteLine("Were any positive experiences you'd like to share? Please give specifics.");
+            Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+       
+   
+        
         }
     }
+}
