f616f1f0bc576d88d945617af6d805aeccb00c73
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-10-29 11:48:53 -0700

    Income Comparison

--- Program.cs
+++ Program.cs
@@ -4,49 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathExercise1
+namespace IncomeComparison
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
-            Console.WriteLine("Please enter a whole number");
-            uint number1 = Convert.ToUInt32(Console.ReadLine());
-            uint product = number1 * 50;
-            Console.WriteLine(product);
+          
+            Console.WriteLine("Anonymous Income Comparison Program");
             Console.ReadLine();
 
-            Console.WriteLine("Please enter another whole number");
-            uint number2 = Convert.ToUInt32(Console.ReadLine());
-            uint total = number2 + 25;
-            Console.WriteLine(total);
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate");
+            int hourlyrate1 = Int32.Parse("20");
+            Console.WriteLine(hourlyrate1);
+            Console.WriteLine("Hours worked per week");
+            int hoursworked1 = Int32.Parse("40");
+            Console.WriteLine(hoursworked1);
             Console.ReadLine();
 
-            Console.WriteLine("Please enter another whole number");
-            double number3 = Convert.ToDouble(Console.ReadLine());
-            double quotient = number3 / 12.5;
-            Console.WriteLine(quotient);
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate");
+            int hourlyrate2 = Int32.Parse("30");
+            Console.WriteLine(hourlyrate2);
+            Console.WriteLine("Hours worked per week");
+            int hoursworked2 = Int32.Parse("40");
+            Console.WriteLine(hoursworked2);
             Console.ReadLine();
 
-            Console.WriteLine("Please enter another whole number");
-            uint number4 = Convert.ToUInt32(Console.ReadLine());
-            bool trueOrFalse = number4 > 50;
-            Console.WriteLine(trueOrFalse.ToString());
+            Console.WriteLine("Annual Salary of Person 1");
+            int annualsalary1 = Int32.Parse("41600");
+            Console.WriteLine(annualsalary1);
             Console.ReadLine();
 
-            Console.WriteLine("And finally, please enter another whole number");
-            double number5 = Convert.ToDouble(Console.ReadLine());
-            double quotient2 = number5 / 7.0;
-            Console.WriteLine(quotient2);
-            double remainder = quotient2 % 2;
-            Console.WriteLine(remainder);
+            Console.WriteLine("Annual Salary of Person 2");
+            int annualsalary2 = Int32.Parse("62400");
+            Console.WriteLine(annualsalary2);
             Console.ReadLine();
 
-
-
-
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool higherSalary = annualsalary1 > annualsalary2;
+            Console.WriteLine(higherSalary);
+            Console.ReadLine();
 
         }
     }
