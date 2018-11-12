a523cd900f93cc071eb7f22833977af31f55c17f
Kayleigh Stenberg <kayleighrstenberg@gmail.com>
2018-11-12 15:38:20 -0800

    methods exercise

---
+++ Methods Exercise/MethodParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Methods_Exercise
+{
+    public class Addition
+    {
+        public int AddTwoValues(int x, int y)
+        {
+            return x + y;
+        }
+        public int AddThreeValues(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public int AddFourValues(int x, int y, int z, int i)
+        {
+            return x + y + z + i;
+        }
+    }
+}
+
