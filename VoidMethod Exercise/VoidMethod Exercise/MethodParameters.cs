63d96d25ed33f4fef9fac17c5e9576b322bc27b7
Kayleigh Stenberg <kayleighrstenberg@gmail.com>
2018-11-13 11:33:28 -0800

    void method exercise

---
+++ VoidMethod Exercise/VoidMethod Exercise/MethodParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod_Exercise
+{
+   public class MethodParameters 
+    {
+      public static int GetMax(int first, int second)
+        {
+            return first > second ? first : second;
+        }
+
+    }
+}
