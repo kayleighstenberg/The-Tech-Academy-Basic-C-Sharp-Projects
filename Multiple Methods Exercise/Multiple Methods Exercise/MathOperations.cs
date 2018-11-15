787091d584d5378e0a1472baf12fcc36a8026df1
Kayleigh Stenberg <kayleighrstenberg@gmail.com>
2018-11-14 20:15:40 -0800

    multiple methods exercise

---
+++ Multiple Methods Exercise/Multiple Methods Exercise/MathOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Methods_Exercise
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+           
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Subtract(double num1, double num2)
+        {
+            return Convert.ToInt32(num1 - num2);
+        }
+
+        public int Add(string one, string two)
+        {
+            int x = 0;
+            int y = 0;
+            if (!Int32.TryParse(one, out x))
+            {
+                x = -1;
+            }
+            if (!Int32.TryParse(two, out y))
+            {
+                y = -1;
+            }
+            return x + y;
+        }
+    }
+}
