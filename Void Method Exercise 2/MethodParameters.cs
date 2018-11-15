5693c1920fa013714b19c7a87fd284ecf0ee1c1d
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-11-14 20:00:10 -0800

    void method exercise 2

---
+++ Void Method Exercise 2/MethodParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod_Exercise
+{
+    public class MethodParameters
+    {
+
+        public void Example2(int x, int y)
+        {
+            int z = x + 10;
+            Console.WriteLine("The second integer value is: " + y);
+        }
+
+    }
+}
---
+++ Void Method Exercise 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod_Exercise
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            int x = 5;
+            int y = 15;
+            MethodParameters test = new MethodParameters();
+            test.Example2(x, y);
+            Console.ReadLine();
+        }
+    }
+}
