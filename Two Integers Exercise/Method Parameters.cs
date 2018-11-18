417ce0bda50043e0432655b0c771810733089ef1
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-11-18 11:53:55 -0800

    test

---
+++ Two Integers Exercise/Method Parameters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Two_Integers_Exercise
+{
+    public class Example
+    {
+
+        public void AddNumbers(int a, [Optional] int[] b)
+        {
+            int total = a + 10;
+            if (b != null)
+            {
+                foreach (int i in b)
+                {
+                    total += i;
+                }
+            }
+            Console.WriteLine("10 + integer one + integer two:");
+            Console.WriteLine(total);    
+        
+        }
+        
+    }
+}
---
+++ Two Integers Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Two_Integers_Exercise
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Please enter an integer.");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second integer (it is not necessary).");
+            int y = 0;
+            try
+            {
+                 y = Convert.ToInt32(Console.ReadLine());
+            }
+            catch 
+            {
+                Console.WriteLine("Nothing was entered for second integer");
+            }
+            
+
+            Example add = new Example();
+            add.AddNumbers(x, new int[] { y });
+            
+            Console.ReadLine();
+
+
+            }
+    }
+}
