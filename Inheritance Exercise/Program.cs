5b7b2e2c2b0e6949a572b8b65a50642d41a6338a
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-11-18 16:05:00 -0800

    inheritance exercise

---
+++ Inheritance Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Wick";
+            person.SayName();
+            
+
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+
+
+}
