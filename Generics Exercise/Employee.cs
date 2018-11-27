bc1b63c458b156eee66a3b5c96e19f2ad75f3847
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-11-26 16:24:22 -0800

    Add files via upload

---
+++ Generics Exercise/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Exercise
+{
+
+    public class Employee<T> 
+    {
+        
+        public List<T> Things { get; set; }
+
+        public string Items { get; set; }
+
+        public int Numbers { get; set; }
+
+    }
+
+        
+
+   
+}
+
+
---
+++ Generics Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Exercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            List<string> Items = new List<string>();
+            Items.Add("Desk");
+            Items.Add("Computer");
+            Items.Add("Mouse");
+            Items.Add("Notebooks");
+            Items.Add("Pens");
+
+            Employee<int> employee2 = new Employee<int>();
+            List<int> Numbers = new List<int>();
+            Numbers.Add(3);
+            Numbers.Add(5);
+            Numbers.Add(4);
+            Numbers.Add(2);
+            Numbers.Add(5);
+
+            foreach (string item in Items)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
