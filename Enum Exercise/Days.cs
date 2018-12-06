e7093738524055f274414f79b94778141d9701f5
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-12-05 20:34:40 -0800

    Add files via upload

---
+++ Enum Exercise/Days.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Exercise
+{
+    enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
+    public class Days
+    {
+
+        public enum Day
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+    }
+}
---
+++ Enum Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Exercise
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+            string dayValue = Console.ReadLine();
+
+            try
+            {
+                Day day = (Day)Enum.Parse(typeof(Day), dayValue);
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
