e9937345ab8df7a8b1ccdc3f242e38f4aaa06e0a
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-12-09 14:14:54 -0800

    Add files via upload

---
+++ Struct Exercise/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Exercise
+{
+    public struct Number
+    {
+        public decimal x;
+
+        public Number(decimal am1)
+        {
+            x = am1;
+        }
+
+    }
+}
---
+++ Struct Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Exercise
+{
+
+    public class Program
+    {
+        static void Main()
+        {
+            Number amount1 = new Number(10.5m);
+
+            Console.WriteLine(amount1.x);
+            Console.ReadLine();
+        }
+    }
+}
