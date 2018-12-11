5c916055ebf9e4cca55508b8785ba2a6cd422625
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-12-10 19:42:30 -0800

    Add files via upload

---
+++ DateTime Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Exercise
+{
+    class Program
+    {
+       public static void Main()
+        {
+            Console.WriteLine("The current Date and Time is: ");
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number.");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours, it will be ", X);
+            double[] hours = { X };
+            DateTime dateValue = DateTime.Now;
+
+            foreach (double hour in hours)
+                Console.WriteLine("{0} + {1} hour(s) = {2}", dateValue, hour, dateValue.AddHours(hour));
+            Console.ReadLine();
+        }
+    }
+}
