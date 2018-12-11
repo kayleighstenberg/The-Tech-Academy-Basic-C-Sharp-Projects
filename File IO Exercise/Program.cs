8d429c370bc107e84162af60008e7008a81bd028
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-12-10 17:50:26 -0800

    Add files via upload

---
+++ File IO Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_Exercise
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] number = new int[] {userInput};
+
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Student\Logs\number.txt"))
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    sw.WriteLine(userInput);
+                }
+            }
+
+            string text = File.ReadAllText(@"C:\Users\Student\Logs\number.txt");
+
+            Console.WriteLine("Your number is: {0}", userInput);
+
+            Console.ReadKey();
+        }
+    }
+}
