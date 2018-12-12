ef7087e8f5ff0665e4eba10976d06491fcb5b5f9
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-12-11 18:09:15 -0800

    Add files via upload

---
+++ Constant, Var Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant__Var_Exercise
+{
+   public class Program 
+    {
+        static void Main()
+        {
+            Student student = new Student();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constant, Var Exercise/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant__Var_Exercise
+{
+   public class Student
+    {
+        public Student() : this("","")
+        {
+        }
+
+        public Student(string name, string city) 
+        {
+            const string studentName = "Kayleigh";
+            var City = "Denver";
+            Console.WriteLine("Student name = {0}, City = {1}", studentName, City);
+        }
+    }
+}
