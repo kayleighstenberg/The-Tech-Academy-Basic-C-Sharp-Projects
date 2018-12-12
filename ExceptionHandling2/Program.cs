2d14530c86508af1f0bc0f5675f3f6e8cbf3cfa1
kayleighstenberg <42855456+kayleighstenberg@users.noreply.github.com>
2018-12-11 19:28:56 -0800

    Add files via upload

---
+++ ExceptionHandling2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling2
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age.");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int birthYear = 2018 - userInput;
+                if (userInput < 1)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine(birthYear);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter a real age.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
